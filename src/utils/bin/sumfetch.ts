d50c7987154990e6928f57db68ceca5b6865b69e
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-05-13 13:15:38 +0300

    sumfetch: update CV link

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -7,7 +7,7 @@ const sumfetch = async (args: string[]): Promise<string> => {
         @@@@               @@@@             -----------  ABOUT
       @@                       @@            ${config.name}
     @@                           @@         ﰩ ${config.ps1_hostname}
-  @@                               @@        <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+  @@                               @@        <u><a href="${config.resume_url}" target="_blank">My up-to-date Resume/CV</a></u>
  @@                         @@@     @@      爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
 @@        @@@                        @@     -----------  CONTACT 
 @@                                   @@      <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
@@ -26,7 +26,7 @@ const sumfetch = async (args: string[]): Promise<string> => {
            ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  sumfetch
         ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------  ABOUT
       ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ${config.name}
-    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            <u><a href="${config.resume_url}" target="_blank">My up-to-date Resume/CV</a></u>
    ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓          爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u> 
 ▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       -----------  CONTACT
 ▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
